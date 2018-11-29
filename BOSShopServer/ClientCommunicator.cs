1938b209b2ba3134cc9c9f6213739c8c8486c8aa
BuildTools <unconfigured@null.spigotmc.org>
2018-11-29 17:50:29 +0100

    Networking - First working version

---
+++ BOSShopServer/ClientCommunicator.cs
@@ -0,0 +1,22 @@
+﻿using Germanen.GUNet.Attributes.Server;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOSShopServer
+{
+    public class ClientCommunicator
+    {
+        [OnClientConnect]
+        public void OnClientConnected(int id)
+        {
+            Console.WriteLine("Client with id " + id + " connected");
+        }
+
+        [OnClientDisconnect]
+        public void OnClientDisconnected(int id)
+        {
+            Console.WriteLine("Client with id " + id + " disconnected");
+        }
+    }
+}
---
+++ BOSShopServer/ServerCore.cs
@@ -0,0 +1,43 @@
+﻿using Germanen.GUNet.Attributes;
+using Germanen.GUNet.Attributes.Default;
+using Germanen.GUNet.Authentication;
+using Germanen.GUNet.Server;
+using Germanen.GUNet.Settings.Server;
+using Germanen.GUNet.Utils;
+using System;
+using System.Net;
+using System.Threading;
+
+namespace BOSShopServer
+{
+    public class ServerCore
+    {
+        public static GUNetServer Server { private set; get; }
+
+        public static IAttributeManager AttributeManager { private set; get; }
+
+        public static WebServer WebServer { private set; get; }
+
+        static void Main(string[] args)
+        {
+            GUN.Init();
+
+            AttributeManager = new AttributeManager();
+
+            Server = new GUNetServer(new GUNServerSettings(AttributeManager, "1.0", IPAddress.Parse("127.0.0.1"), 8888, false, 12, 12, 0, true, new NullAuthenticator()));
+
+            Server.StartServer();
+
+            WebServer = new WebServer();
+
+            AttributeManager.AddClass(new ClientCommunicator());
+
+            while (true)
+            {
+                Server.HandleNetworkingData();
+                Console.Title = "Server - " + Server.Clients.Count;
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
